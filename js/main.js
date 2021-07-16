1928bf3cdc13ea06cb0195138d8db2bbda1968a3
Nello Castello <dfarino46@gmail.com>
2021-07-16 18:19:57 +0300

    Изменен стиль исправления прошлого бага

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function showPopForImageOfNewsItem(parent) {
+    parent.querySelector('.news-item__image__pop-up').style.visibility = 'visible';
+}
+function hidePopForImageOfNewsItem(parent) {
+    parent.querySelector('.news-item__image__pop-up').style.visibility = 'hidden';
+}
